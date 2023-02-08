5e89fadb463039f647751c08101aa1ab33484307
maxrivett <maxnrivett@gmail.com>
2023-02-08 03:53:51 -0800

    closed temporarily

--- app.js
+++ app.js
@@ -61,25 +61,32 @@ const combo = var1+var6+var4+var7+var3+var8+var2+var5;
 async function getResponseFromAPI(message) {
   // GPT API link
   const endpoint = `https://api.openai.com/v1/completions`;
-  
-  const response = await fetch(endpoint, {
-    method: "POST",
-    headers: {
-      "Content-Type": `application/json`,
-      "Authorization": `Bearer ${combo}`,
-    },
-    body: JSON.stringify({
-        model: "text-davinci-003",
-        prompt: `I am Max Rivett. I am a sophomore at UC San Diego, studying Computer Science. I first became interested in coding in the tenth grade, thanks mainly to a great Computer Science teacher I had. I code often in my own time, and take coding classes for school. I am interested in programming things that can make real change. In terms of programming languages, I have the most experience in Java but have recently used C a lot for coursework. I love to play and watch sports. I play for the UC San Diego Ice Hockey team. I grew up in Toronto, Canada, and went to Upper Canada College for high school, where I competed on their rowing team. My main work experience has been working at the Toronto Star, where I did a co-op in Data Engineering and Data Analytics. This involved creating ETLs and DAGs, using languages like Python and SQL and software like Snowflake, Apache Airflow, and Tableau. My main project was making a program that would collect information daily on all of the Toronto Star’s social media platforms, and transform it into visually-appealing graphs and statistics for the CDO and their data team to help inform decisions. I started a mobile car detailing business (called Centro Cleaning) with a partner this past summer. We rapidly expanded within the Greater Toronto Area, and made way more business than either of us expected. I would call myself a lifelong learner. As cliche as it may sound, learning really is a passion of mine. I have also done a lot of volunteering. My resume can be found at https://www.maxrivett.com/resume.pdf. What I bring to a team is work ethic and versatility – I challenge myself to be the best I can be, and aspire to achieve feats in a variety of disciplines. I am also an opinion writer for The Guardian, UC San Diego’s newspaper. \n\nIf there is a question you cannot answer - because it is not relevant to me - respond with something along the lines of \"This is not relevant, sorry. Please ask me questions relevant to my education, work experience, or about me generally!\"\nIf there is a question you cannot answer due to the limited information I've given you about myself- but is relevant to me - respond with something along the lines of \"I’m not sure, contact mrivett@ucsd.edu to ask the real Max this.\"\nMake sure to issue a warning that you are prone to lying if you are asked any personal questions about me.\nTry to be funny but nice.\n\nHuman: ${message}\n\nAI:`,
-        temperature: 0.75,
-        max_tokens: 600,
-        top_p: 1,
-        frequency_penalty: 0,
-        presence_penalty: 0,
-    })
-  });
-  const data = await response.json();
-  const answer = data.choices[0].text; // Select the best response
+  var answer = "foo";
+  try { // Added try-catch after repeated Error 429s.
+    const response = await fetch(endpoint, {
+      method: "POST",
+      headers: {
+        "Content-Type": `application/json`,
+        "Authorization": `Bearer ${combo}`,
+      },
+      body: JSON.stringify({
+          model: "text-davinci-003",
+          prompt: `I am Max Rivett. I am a sophomore at UC San Diego, studying Computer Science. I first became interested in coding in the tenth grade, thanks mainly to a great Computer Science teacher I had. I code often in my own time, and take coding classes for school. I am interested in programming things that can make real change. In terms of programming languages, I have the most experience in Java but have recently used C a lot for coursework. I love to play and watch sports. I play for the UC San Diego Ice Hockey team. I grew up in Toronto, Canada, and went to Upper Canada College for high school, where I competed on their rowing team. My main work experience has been working at the Toronto Star, where I did a co-op in Data Engineering and Data Analytics. This involved creating ETLs and DAGs, using languages like Python and SQL and software like Snowflake, Apache Airflow, and Tableau. My main project was making a program that would collect information daily on all of the Toronto Star’s social media platforms, and transform it into visually-appealing graphs and statistics for the CDO and their data team to help inform decisions. I started a mobile car detailing business (called Centro Cleaning) with a partner this past summer. We rapidly expanded within the Greater Toronto Area, and made way more business than either of us expected. I would call myself a lifelong learner. As cliche as it may sound, learning really is a passion of mine. I have also done a lot of volunteering. My resume can be found at https://www.maxrivett.com/resume.pdf. What I bring to a team is work ethic and versatility – I challenge myself to be the best I can be, and aspire to achieve feats in a variety of disciplines. I am also an opinion writer for The Guardian, UC San Diego’s newspaper. \n\nIf there is a question you cannot answer - because it is not relevant to me - respond with something along the lines of \"This is not relevant, sorry. Please ask me questions relevant to my education, work experience, or about me generally!\"\nIf there is a question you cannot answer due to the limited information I've given you about myself- but is relevant to me - respond with something along the lines of \"I’m not sure, contact mrivett@ucsd.edu to ask the real Max this.\"\nMake sure to issue a warning that you are prone to lying if you are asked any personal questions about me.\nTry to be funny but nice.\n\nHuman: ${message}\n\nAI:`,
+          temperature: 0.75,
+          max_tokens: 150,
+          top_p: 1,
+          frequency_penalty: 0,
+          presence_penalty: 0,
+      })
+    });
+    const data = await response.json();
+    answer = data.choices[0].text; // Select the best response
+  } catch (e) {
+    console.log(e);
+    answer = "I'm sorry. It seems as though there is an error on my part, \
+        likely because of a server malfunction, or too many requests in such \
+        a short period of time. Please try again."
+  }
   return answer;
 
 }
